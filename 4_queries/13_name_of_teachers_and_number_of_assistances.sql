e338ba2712e29a8393c5e71a38dc3a8bf3e8e175
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-01-05 19:59:02 +0000

    Name of Teachers and Number of Assistances

--- 4_queries/13_name_of_teachers_and_number_of_assistances.sql
+++ 4_queries/13_name_of_teachers_and_number_of_assistances.sql
@@ -5,4 +5,5 @@ JOIN students ON assistance_requests.student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
 WHERE cohorts.name = 'JUL02'
 GROUP BY teacher, cohort
+--GROUP BY teachers.name, cohorts.name
 ORDER BY teacher;
