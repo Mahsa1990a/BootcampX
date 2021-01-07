769cb89a075ac6720c2feedd74559c460adcd108
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-01-07 02:29:56 +0000

    Parameterized Query for protection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,14 +8,14 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-//query id, name, cohort_id from students
+//                                      query id, name, cohort_id from students
 // pool.query(`       
 //   SELECT id, name, cohort_id
 //   FROM students
 //   LIMIT 5;
 // `) 
 
-//query id, name from students and name from cohorts
+//                                       query id, name from students and name from cohorts
 // pool.query(`
 //   SELECT students.id, students.name AS student, cohorts.name AS cohort
 //   FROM students
@@ -23,20 +23,44 @@ const pool = new Pool({
 //   LIMIT 5;
 // `)
 
-//Adjust code ro run node students.js FEB 2 -> cohort name=FEB, limit = 2
-pool.query(`         
+//                                       Adjust code ro run node students.js FEB 2 -> cohort name=FEB, limit = 2
+// pool.query(`         
+//   SELECT students.id, students.name AS student, cohorts.name AS cohort
+//   FROM students
+//   JOIN cohorts ON cohort_id = cohorts.id
+//   WHERE cohorts.name LIKE '%${process.argv[2]}%'
+//   LIMIT ${process.argv[3] || 5};
+// `)
+// .then(res => {
+//   //console.log('this is res: ', res); //answer we want is inside res.rows
+//   //console.log('This is expected answer res.rows: ', res.rows); //arr of obj, result is same as we get from psql
+//   res.rows.forEach(user => {
+//     console.log(user); //5 user objs
+//     //console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`); //5 of them
+//     //console.log(`${user.student} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+//   })
+// }).catch(err => console.error('query error', err.stack));
+
+
+const queryString = `
   SELECT students.id, students.name AS student, cohorts.name AS cohort
   FROM students
-  JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
-  LIMIT ${process.argv[3] || 5};
-`)
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`;
+
+//The values that come from somewhere else:
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+
+
+pool.query(queryString, values)
 .then(res => {
-  //console.log('this is res: ', res); //answer we want is inside res.rows
-  //console.log('This is expected answer res.rows: ', res.rows); //arr of obj, result is same as we get from psql
   res.rows.forEach(user => {
-    //console.log(user); //5 user objs
-    //console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`); //5 of them
+    //console.log(user);
     console.log(`${user.student} has an id of ${user.id} and was in the ${user.cohort} cohort`);
   })
 }).catch(err => console.error('query error', err.stack));
